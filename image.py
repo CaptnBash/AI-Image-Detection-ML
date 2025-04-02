6d5f6ed93e5d483b612df664d7913876425af51f
Samuel Kuhn <samuel.kuhn@yoga>
2025-04-02 20:13:26 +0200

    some changes

--- image.py
+++ image.py
@@ -1,16 +1,29 @@
+import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import os
 
-img = cv2.imread('img.JPG')
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def compute_histogram(image_path, bins=256):
+    img = cv2.imread(image_path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-hist_r = cv2.calcHist([img_rgb], [0], None, [256], [0, 256])
-hist_g = cv2.calcHist([img_rgb], [1], None, [256], [0, 256])
-hist_b = cv2.calcHist([img_rgb], [2], None, [256], [0, 256])
+    hist_r = cv2.calcHist([img_rgb], [0], None, [bins], [0, 256]).flatten()
+    hist_g = cv2.calcHist([img_rgb], [1], None, [bins], [0, 256]).flatten()
+    hist_b = cv2.calcHist([img_rgb], [2], None, [bins], [0, 256]).flatten()
+    return np.concatenate([hist_r, hist_g, hist_b])
 
-# Plot histograms
-plt.plot(hist_r, color='red', label='Red')
-plt.plot(hist_g, color='green', label='Green')
-plt.plot(hist_b, color='blue', label='Blue')
-plt.legend()
-plt.show(block=True)
+image_paths = os.listdir("images")
+
+histograms = np.array([compute_histogram(f"images/{path}") for path in image_paths])
+y = ["real"] * len(histograms)
+# np.save("histograms.npy", histograms)
+
+X_train, X_test, y_train, y_test = train_test_split(histograms, y, test_size=0.2)
+clf = RandomForestClassifier().fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+
+score = accuracy_score(y_test, predictions)
+print(score)
